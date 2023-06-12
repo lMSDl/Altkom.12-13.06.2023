d382884fb266d0f1abbd281214bbdf46171695d4
Paweł Łęczycki <pawel@msd.info.pl>
2023-06-12 11:27:50 +0200

    ciągi znaków (string) - deklaracja, inicjalizacja i przypisywanie wartości

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,18 @@
 ﻿
-Console.WriteLine("Hello, World!");
+
+//deklaracja zmiennej lokalnej o typie string (ciąg znaków) i nazwie helloVariable
+string helloVariable;
+//nie możemy używać zmiennej, która nie została zainicjalizowana
+
+//inicjalizacja zmiennej - pierwsze przypisanie wartości (może być wartością null)
+helloVariable = "Hello";
+
+Console.WriteLine(helloVariable);
+
+//deklaracja z inicjalizacją
+string worldString = "world";
+Console.WriteLine(worldString);
+
+//przypisanie wartości
+worldString = "World";
+
