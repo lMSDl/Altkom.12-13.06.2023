cf4fe8d0a49f52a15ebffefaa900bb14cdcd9815
Paweł Łęczycki <pawel@msd.info.pl>
2023-06-12 11:55:42 +0200

    Łączenie stringów

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,6 +7,7 @@ string helloVariable;
 //inicjalizacja zmiennej - pierwsze przypisanie wartości (może być wartością null)
 helloVariable = "Hello";
 
+//instrukcje kończymy znakiem średnika
 Console.WriteLine(helloVariable);
 
 //deklaracja z inicjalizacją
@@ -16,3 +17,31 @@ Console.WriteLine(worldString);
 //przypisanie wartości
 worldString = "World";
 
+
+string helloOuput;
+
+//łączenie stringów za pomocą +
+helloOuput = helloVariable + " " + worldString + "!";
+Console.WriteLine(helloOuput);
+
+//łączenie stringów za pomocą funkcji Concat (pparametry wypisane po przecinku)
+helloOuput = string.Concat(helloVariable, " ", worldString, "!");
+Console.WriteLine(helloOuput);
+
+//łączenie stringów za pomocą funkcji format, gdzie przekazujemy jak ma wyglądać string i z jakich elementów ma się składać ( {0} - index paramtru, liczony od 0 )
+
+//helloOuput = string.Format("{0} {1}!", helloVariable, worldString);
+string helloFormat = "{0} {1}!";
+helloOuput = string.Format(helloFormat, helloVariable, worldString);
+Console.WriteLine(helloOuput);
+
+helloOuput = string.Format(helloFormat, "Ala", "ma kota");
+Console.WriteLine(helloOuput);
+
+//łączenie stringów z wykorzystaniem interpolacji (string interpolation)
+helloOuput = $"{helloVariable} {worldString}!";
+Console.WriteLine(helloOuput);
+
+//łączenie stringów za pomocą funkcji Join (pierwszy parametr to separator)
+helloOuput = string.Join(" -^- ", helloVariable, worldString, "ala", "makota");
+Console.WriteLine(helloOuput);
